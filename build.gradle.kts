74daad0fef7dca5884e0054f84fd3da1e2ba50f3
Naoki-Hidaka <naoki.hidaka@dena.com>
2025-03-20 00:37:24 +0900

    fix

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,18 +2,4 @@ plugins {
     alias(libs.plugins.kotlin.jvm) apply false
     alias(libs.plugins.kotlin.serialization) apply false
     alias(libs.plugins.plugin.publish) apply false
-    alias(libs.plugins.kotlin.android) apply false
-    alias(libs.plugins.android.application) apply false
-//    id("io.github.dosukoi-juku.licenses-plugin") version "0.0.1-SNAPSHOT" apply false
-}
-
-buildscript {
-    repositories {
-        google()
-        mavenCentral()
-        mavenLocal()
-    }
-    dependencies {
-        classpath("com.google.android.gms:oss-licenses-plugin:0.10.6")
-    }
 }
